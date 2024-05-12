e8c31291fb1bb0516808988b2a4f4726bf3b446a
Theshikanavod <theshikanavod@gmail.com>
2024-05-12 11:04:02 +0530

    ui updated

--- src/ClientUI.java
+++ src/ClientUI.java
@@ -26,10 +26,41 @@ public class ClientUI {
         mainPanel = new JPanel(new BorderLayout());
         mainPanel.setBackground(Color.WHITE);
 
-        nameLabel = new JLabel();
+        // Left Panel with gradient background
+        JPanel leftPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                int w = getWidth();
+                int h = getHeight();
+                Color color1 = new Color(51, 153, 255); // Light Blue
+                Color color2 = new Color(0, 102, 204); // Dark Blue
+                GradientPaint gp = new GradientPaint(0, 0, color1, w, h, color2);
+                g2d.setPaint(gp);
+                g2d.fillRect(0, 0, w, h);
+            }
+        };
+        leftPanel.setPreferredSize(new Dimension(150, frame.getHeight()));
+        leftPanel.setLayout(new BorderLayout());
+
+        JLabel groupChatLabel = new JLabel("Group Chat", SwingConstants.CENTER);
+        groupChatLabel.setForeground(Color.WHITE);
+        groupChatLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        leftPanel.add(groupChatLabel, BorderLayout.CENTER);
+
+        // Initialize nameLabel here
+        nameLabel = new JLabel(); // <-- Initialization
+        nameLabel.setForeground(Color.WHITE);
         nameLabel.setFont(new Font("Arial", Font.BOLD, 14));
-        nameLabel.setHorizontalAlignment(SwingConstants.LEFT);
-        mainPanel.add(nameLabel, BorderLayout.NORTH); // Add the name label to the top of the UI
+        leftPanel.add(nameLabel, BorderLayout.NORTH); // Add the nameLabel to the top of the UI
+
+        // Icon
+        ImageIcon icon = new ImageIcon("icon.png"); // Replace "icon.png" with the path to your icon
+        JLabel iconLabel = new JLabel(icon, SwingConstants.CENTER);
+        leftPanel.add(iconLabel, BorderLayout.SOUTH);
+
+        mainPanel.add(leftPanel, BorderLayout.WEST);
 
         chatArea = new JTextArea();
         chatArea.setEditable(false);
